f916c89176eb24085528549aca8e79d4689450d5
15sharmanitin <15sharmanitin@gmail.com>
2022-03-03 21:01:56 -0500

    secondclass

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,5 +5,6 @@ public class Main {
     public static void main(String[] args) {
 	// write your code here
         System.out.println("firstclass ");
+        System.out.println("secondclass ");
     }
 }
